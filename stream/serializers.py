2998e48f0244b2dc1ab282ae4bf8c434d8823f73
tommcn <tommcn@mcnamer.ca>
2021-11-07 14:01:55 -0500

    Entry logic

--- stream/serializers.py
+++ stream/serializers.py
@@ -1,11 +1,32 @@
+import bson
+
 from rest_framework import serializers
 
-from .models import Stream
+from .models import Stream, Entry
 from .validators import FieldsValueValidator
 
 
-# Stream serializer
+class ObjectIDField(serializers.Field):
+    def to_representation(self, value):
+        return str(value)
+
+    def to_internal_value(self, data):
+        return bson.ObjectId(data)
+
+
+# Entry serializer
+class EntrySerializer(serializers.ModelSerializer):
+    _id = ObjectIDField()
+    data = serializers.JSONField()
+
+    class Meta:
+        model = Entry
+        fields = ("_id", "data")
+        read_only_fields = ("_id",)
+
+
 class StreamSerializer(serializers.ModelSerializer):
+    _id = ObjectIDField()
     fields = serializers.JSONField(validators=[FieldsValueValidator])
 
     class Meta:
--- stream/views.py
+++ stream/views.py
@@ -1,12 +1,11 @@
 import bson
 from rest_framework import viewsets
 
-from stream.models import Stream
-from stream.serializers import StreamSerializer
+from stream.models import Entry, Stream
+from stream.serializers import EntrySerializer, StreamSerializer
+
 
 # Create your views here.
-
-
 class StreamViewset(viewsets.ModelViewSet):
     queryset = Stream.objects.all()
     serializer_class = StreamSerializer
@@ -15,5 +14,20 @@ class StreamViewset(viewsets.ModelViewSet):
         queryset = self.get_queryset()
         # Remember to convert the `pk` argument from `str` to `ObjectId`
         # since Mongo expects it to be that way.
+        obj = queryset.filter(pk=bson.ObjectId(self.kwargs["pk"])).first()
+        return obj
+
+
+class EntryViewset(viewsets.ModelViewSet):
+    queryset = Entry.objects.none()
+    serializer_class = EntrySerializer
+
+    def get_queryset(self):
+        s = Stream.objects.get(pk=bson.ObjectId(self.kwargs["stream_pk"]))
+        objs = Entry.objects.filter(stream=s)
+        return objs
+
+    def get_object(self):
+        queryset = self.get_queryset()
         obj = queryset.filter(pk=bson.ObjectId(self.kwargs["pk"])).first()
         return obj
